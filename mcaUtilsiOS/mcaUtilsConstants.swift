fd17aedaab5f54f0ef886ac06df9714820679e5a
isai_18k <isai.garcia.rys@gmail.com>
2019-09-06 13:54:02 -0500

    -Initial Commite

---
+++ mcaUtilsiOS/mcaUtilsConstants.swift
@@ -0,0 +1,17 @@
+//
+//  mcaUtilsConstants.swift
+//  mcaUtilsiOS
+//
+//  Created by Pilar del Rosario Prospero Zeferino on 12/12/18.
+//  Copyright © 2018 Roberto. All rights reserved.
+//
+
+import UIKit
+
+class mcaUtilsConstants: NSObject {
+    
+    static let GOOGL_API_KEY_CL = "AIzaSyDXAiAgDYIY1WQKbxE-IQ1zcYZyruafKGM"
+    static let GOOGL_API_KEY_CENAM = "AIzaSyD08RUplL80w7untwrLwQTQfpF_dSUYB_M"
+    static let BUNDLE_IDENTIFIER = "com.miclaro.app.mcaUtilsiOS"
+
+}
